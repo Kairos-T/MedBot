efd7abc00bd2a913d208897a3d94d9cfb26e665d
Kairos-T <hestiatay@gmail.com>
2024-03-04 21:56:05 +0800

    feat: added simplified bot logic

--- bot.py
+++ bot.py
@@ -1,23 +1,84 @@
 import os
+from datetime import datetime
 import discord
 from dotenv import load_dotenv
-from discord.ext import commands
+from discord.ext import commands, tasks
+import json
+
+# Bot Initialisation
+
 
 load_dotenv()
-
 token = os.environ.get('DISCORD_TOKEN')
-print(token)
-
 intents = discord.Intents.default()
 intents.messages = True
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents)
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+# File Initialisation
+
+REMINDER_FILE = 'reminders.json'
+
+
+def load_reminders():
+    if os.path.exists(REMINDER_FILE) and os.path.getsize(REMINDER_FILE) > 0:
+        with open(REMINDER_FILE, 'r') as f:
+            return json.load(f)
+    return {}
+
+
+def save_reminders(reminders):
+    with open(REMINDER_FILE, 'w') as f:
+        json.dump(reminders, f, indent=2)
+
+# Checker
+
+
+@tasks.loop(seconds=10)
+async def check_reminders():
+    now = datetime.now().strftime("%H:%M")
+    print("Loading reminders: " + now)
+    reminders = load_reminders()
+    now_datetime = datetime.strptime(now, "%H:%M")
+
+    for reminder in reminders:
+        reminder_time = datetime.strptime(reminder["time"], "%H:%M")
+        if reminder_time <= now_datetime:
+            channel = bot.get_channel(reminder["channel"])
+            await channel.send(f"Reminder for <@{reminder['author']}>: {reminder['reminder']}")
+            reminders.remove(reminder)
+
+    save_reminders(reminders)
+
+
+# Events
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+    check_reminders.start()
 
-    bot.load_extension('cogs.reminders')
+# Commands
+
+
+@bot.command()
+async def add(ctx, time, *, message: str):
+    try:
+        reminder_time = datetime.strptime(time, "%H:%M").strftime("%H:%M")
+    except ValueError:
+        await ctx.send("Invalid time format. Please use HH:MM")
+        return
+
+    reminders = load_reminders()
+    reminders.append({
+        "time": reminder_time,
+        "reminder": message,
+        "channel": ctx.channel.id,
+        "author": ctx.author.id
+    })
+    save_reminders(reminders)
+
+    await ctx.send(f"Reminder added for {reminder_time} - {message}")
+
 
 bot.run(token)
